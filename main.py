6800a84a3e12df5a4a977744658f1a0b98a88916
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-26 18:38:28 +1000

    working api call

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import requests
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
+STOCK_API_KEY=""
+
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+print(response)
 
 
 """
