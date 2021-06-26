eca701292555a9e9ed0cc3c5dad2f3397deab3c6
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-26 19:31:06 +1000

    completed project

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
-import os
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-
+STOCK_API_KEY = ""
+NEWS_API_KEY = ""
+TWILIO_SID = ""
+TWILIO_AUTH_TOKEN = ""
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
@@ -21,36 +24,39 @@ data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 yesterdays_data = data_list[0]
 yesterdays_closing_price = yesterdays_data["4. close"]
-print(yesterdays_closing_price)
 
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
-print(day_before_yesterday_closing_price)
 
-difference = abs(float(yesterdays_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
+difference = float(yesterdays_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down ="🔼"
+else:
+    up_down = "🔽"
 
-diff_percent = (difference / float(yesterdays_closing_price)) * 100
-print(diff_percent)
+diff_percent = round(difference / float(yesterdays_closing_price) * 100)
 
-if diff_percent > 1:
+if abs(diff_percent) > 1:
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
     }
     news_reponse = requests.get(NEWS_ENDPOINT, params=news_params)
     articles = news_reponse.json()["articles"]
-    print(articles)
 
     three_articles = articles[:3]
-    print(three_articles)
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+
+    formatted_articles = [f"{STOCK_NAME} {up_down}{abs(diff_percent)}\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_='',
+            to=''
+        )
+
+
 
